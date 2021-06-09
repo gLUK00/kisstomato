83052bd46e63842bc621168b3455a0cbeffa60bc
Hidalgo Emmanuel <ehidalgo@macif.fr>
2021-06-09 16:33:02 +0200

    aa

---
+++ svb/src/components/pages/project.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>page detail project</h1>
+  </div>
+</template>
+
+<script>
+export default { name: 'PageProject' }
+</script>
---
+++ svb/src/components/pages/root.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="root_message">message</div>
+    <b-row>
+      <b-col>col</b-col>
+      <b-col>col</b-col>
+      <b-col>col</b-col>
+      <b-col>col</b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+  .root_message{
+    border: 1px solid red;
+    border-radius: 20px;
+  }
+</style>
