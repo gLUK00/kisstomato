8e62f49f18f0607f0bfccfe0373c056a3013ed44
hidalgo <hidalgoemmanuel@gmail.com>
2023-09-05 21:52:53 +0200

    =dev

--- eel/core/generator.py
+++ eel/core/generator.py
@@ -1,4 +1,6 @@
-import os, glob
+import os, glob, shutil
+
+from pathlib import Path
 
 # recupere les informations des projets
 def getNodeById( sId, oNode ):
@@ -21,13 +23,58 @@ def getNodeById( sId, oNode ):
 
 # merge de repertoire
 def mergeDirs( sDirSource, sDirTarget, oConfig=None ):
+    sRapport = ''
 
-    # recupere l'ensemble des fichiers et repertoires de la source
-    oAllSrc = glob.glob( sDirSource + os.sep + '**' )
+    # recupere l'ensemble des fichiers et repertoires source et cible
+    oAllSrc = glob.glob( sDirSource + os.sep + '**', recursive=True )
+    oAllTar = glob.glob( sDirTarget + os.sep + '**', recursive=True )
+
+    # pour tous les elements sources
+    for sElement in oAllSrc:
+
+        # deduction du nom cible
+        sElementTarget = sDirTarget + sElement[ len( sDirSource ) : ]
+
+        # si c'est un repertoire et qu'il n'est pas present en cible
+        if os.path.isdir( sElement ):
+
+            # le repertoire existe deja en cible
+            if sElementTarget in oAllTar:
+                continue
+
+            sRapport += 'Creation du repertoire : ' + sElementTarget + '\n'
+            os.makedirs( sElementTarget, mode = 0o777 )
+            continue
+
+        # determine l'extension du fichier
+        sExt = ''
+        iPosPoint = sElement.rfind( '.' )
+        if iPosPoint != -1:
+            sExt = sElement[ iPosPoint + 1 : ].lower()
+
+        # si le fichier n'existe pas en cible ou n'est pas a prendre en compte dans la configuration
+        if not sElementTarget in oAllTar or oConfig == None or not sExt in oConfig.keys():
+
+            # copie du fichier
+            sRapport += 'Copie du fichier : ' + sElementTarget + '\n'
+            shutil.copyfile( sElement, sElementTarget )
+            continue
+        
+        # recuperation du contenu source et cible
+        sContentSrc = Path( sElement ).read_text()
+        sContentTar = Path( sElementTarget ).read_text()
+
+        print( 'ext' )
+        print( sExt )
+        print( sContentSrc )
+        print( sContentTar )
+
+
 
     print( sDirSource )
     print( sDirTarget )
     print( oConfig )
-    print( oAllSrc )
+    #print( oAllSrc )
+    #print( oAllTar )
 
-    return 'in mergeDirs'
+    return sRapport
